23a29fbedd732cce39f7a591c0c6ffcb45a08052
GuilhermeGomes00 <guigomes0307@gmail.com>
2023-07-24 19:42:59 -0300

    Primeiro commit

---
+++ Readme.md
@@ -0,0 +1,5 @@
+Marca-Ponto (marcador de horário)
+
+
+
+Esse projeto tem o objetivo de relacionar o horário da pessoa e guardar em um banco de dados SQL.
---
+++ main.py
@@ -0,0 +1,48 @@
+# Libs
+
+import mysql.connector
+import datetime
+
+# --------------------------------------------
+# connection
+
+conexao = mysql.connector.connect(
+    host="localhost", user="root", password="10203040Gu!s", database="horario"
+)
+cursor = conexao.cursor()  # Conexão realizada aqui
+
+# --------------------------------------------------
+# Logic
+
+
+def arm():
+    while True:
+        pergunta = input(
+            "Qual horario deseja marcar? \nEntrada \nInicio do almoço \nSaida do almoço \nSaida \n Nenhum \n>"
+        )
+        pergunta = pergunta.lower()
+        if pergunta == "Entrada":
+            nome = input("Qual seu nome? ")
+            entrada = datetime.datetime.now()  # entrada.strftime("%H:%M:%S")
+            break
+
+
+# --------------------------------------------------
+# Commands
+
+
+# command = f"INSERT INTO Pessoas (nome) VALUES ('{}]');"
+cursor.execute(command)
+conexao.commit()
+
+
+"""
+
+    nome = input("Qual seu nome? ")
+    dia = datetime.datetime.now() # dia.strftime("%Y/%m/%d")
+    entrada = datetime.datetime.now() # entrada.strftime("%H:%M:%S")
+    entrada_almoco = datetime.datetime.now() # .strftime("%H:%M:%S")
+    saida_almoco = datetime.datetime.now() #.strftime("%H:%M:%S")
+    saida = datetime.datetime.now() # .strftime("%H:%M:%S")
+
+"""
