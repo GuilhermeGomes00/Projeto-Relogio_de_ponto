37c2c9eb0afbece5d71a3ab82c12017e76ba1c95
GuilhermeGomes00 <guigomes0307@gmail.com>
2023-07-28 01:08:52 -0300

    Relogio de ponto

--- main.py
+++ main.py
@@ -10,18 +10,24 @@ conexao = mysql.connector.connect(
     host="localhost", user="root", password="10203040Gu!s", database="horario"
 )
 cursor = conexao.cursor()  # Conexão realizada aqui
+print('-------------------------------------')
+print('Conectando-se ao servidor...')
+print('-------------------------------------')
 
 # --------------------------------------------------
 # Logic
 
 while True:
-    nome_funcionario = input("Qual seu nome?")
+    print('-------------------------------------')
+    nome_funcionario = input("Qual seu nome? ")
+    print('-------------------------------------')
     nome_funcionario = nome_funcionario.lower()
 
     pergunta = input(
         f"Bom dia, {nome_funcionario}! Qual ponto deseja marcar? "
-        + "\nEntrada \nInicio do almoço \nSaida do almoço \nSaida \nNenhum \n>"
+        + "\nEntrada \nInicio do almoço \nSaida do almoço \nSaida \nNenhum \n> "
     )
+    print('-------------------------------------')
     pergunta = pergunta.lower()
 
     if pergunta == "entrada":
@@ -43,23 +49,33 @@ while True:
         atualizar_sql = (f"UPDATE marca_ponto SET InicioAlmoco = '{inicio_almoco}' Where nome = '{nome_funcionario}'")
         cursor.execute(atualizar_sql)
         conexao.commit()
-        print("Inicio do almoço atualizado com sucesso! Tenha um ótimo descanso.")
+        print(
+            f"Inicio do almoço atualizado com sucesso! Tenha um ótimo descanso, {nome_funcionario}."
+        )
         break
 
-# --------------------------------------------------
-# Commands
+    elif pergunta == "saida do almoço":
+        saida_almoco = datetime.datetime.now()
+        saida_almoco = saida_almoco.strftime("%H:%M:%S")
 
-# cursor.execute(command)
-# conexao.commit()
+        atualizar_sql = (f"UPDATE marca_ponto SET SaidaAlmoco = '{saida_almoco}' WHERE nome = '{nome_funcionario}'")
+        cursor.execute(atualizar_sql)
+        conexao.commit()
+        print(f"Inicio do almoço atualizado com sucesso! Espero que tenha tido uma ótima refeição {nome_funcionario}.")
+        break
 
+    elif pergunta == "saida":
+        saida = datetime.datetime.now()
+        saida = saida.strftime("%H:%M:%S")
 
-"""
+        atualizar_sql = (f"UPDATE marca_ponto SET HoraSaida = '{saida}' WHERE nome = '{nome_funcionario}'")
+        cursor.execute(atualizar_sql)
+        conexao.commit()
+        print(f"Saida atualizado com sucesso! Até mais, {nome_funcionario}!")
+        break
 
-    nome = input("Qual seu nome? ")
-    dia = datetime.datetime.now() # dia.strftime("%Y/%m/%d")
-    entrada = datetime.datetime.now() # entrada.strftime("%H:%M:%S")
-    entrada_almoco = datetime.datetime.now() # .strftime("%H:%M:%S")
-    saida_almoco = datetime.datetime.now() #.strftime("%H:%M:%S")
-    saida = datetime.datetime.now() # .strftime("%H:%M:%S")
+    else:
+        print('-------------------------------------')
+        print('ops... Algum erro ocorreu, por favor, tente novamente')
+        print('-------------------------------------')
 
-"""
